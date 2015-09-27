c959f9c0e82bc514b50d475c160483977c1c21a5
shamanping <faqyou16@gmail.com>
2015-09-27 21:37:46 +0700

    Add project

---
+++ Source/Galaxy.Environments/Actors/Ultron.cs
@@ -0,0 +1,41 @@
+﻿#region using
+
+using System;
+using System.Diagnostics;
+using System.Windows;
+using Galaxy.Core.Actors;
+using Galaxy.Core.Environment;
+using Point = System.Drawing.Point;
+using Size = System.Drawing.Size;
+
+#endregion
+
+namespace Galaxy.Environments.Actors
+{
+    public class Ultron : Ship
+    {
+
+    #region Constructors
+
+    public Ultron(ILevelInfo info)
+        : base(info)
+    {
+      Width = 30;
+      Height = 30;
+      ActorType = ActorType.Enemy;
+    }
+
+    #endregion
+
+    #region Overrides
+
+    public override void Load()
+    {
+        base.Load();
+        Load(@"Assets\ultron.png");
+    }
+
+    #endregion
+
+    }
+}
---
+++ Source/Galaxy.Environments/LevelOne.cs
@@ -0,0 +1,122 @@
+﻿#region using
+
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using Galaxy.Core.Actors;
+using Galaxy.Core.Collision;
+using Galaxy.Core.Environment;
+using Galaxy.Environments.Actors;
+
+#endregion
+
+namespace Galaxy.Environments
+{
+  /// <summary>
+  ///   The level class for Open Mario.  This will be the first level that the player interacts with.
+  /// </summary>
+  public class LevelOne : BaseLevel
+  {
+    private int m_frameCount;
+
+    #region Constructors
+
+    /// <summary>
+    ///   Initializes a new instance of the <see cref="LevelOne" /> class.
+    /// </summary>
+    public LevelOne()
+    {
+      // Backgrounds
+      FileName = @"Assets\LevelOne.png";
+
+      // Enemies
+      for (int i = 0; i < 5; i++)
+      {
+        var ship = new Ship(this);
+        int positionY = ship.Height + 10;
+        int positionX = 150 + i * (ship.Width + 50);
+
+        ship.Position = new Point(positionX, positionY);
+
+        Actors.Add(ship);
+      }
+
+      //Ultron
+      for (int i = 0; i < 1; i++)
+      {
+          var ultron = new Ultron(this);
+          int positionY = ultron.Height + 90;
+          int positionX = 300 + i * (ultron.Width + 50);
+
+          ultron.Position = new Point(positionX, positionY);
+
+          Actors.Add(ultron);
+      }
+
+      // Player
+      Player = new PlayerShip(this);
+      int playerPositionX = Size.Width / 2 - Player.Width / 2;
+      int playerPositionY = Size.Height - Player.Height - 50;
+      Player.Position = new Point(playerPositionX, playerPositionY);
+      Actors.Add(Player);
+    }
+
+    #endregion
+
+    #region Overrides
+
+    private void h_dispatchKey()
+    {
+      if (!IsPressed(VirtualKeyStates.Space)) return;
+
+      if(m_frameCount % 10 != 0) return;
+
+      Bullet bullet = new Bullet(this)
+      {
+        Position = Player.Position
+      };
+
+      bullet.Load();
+      Actors.Add(bullet);
+    }
+
+    public override BaseLevel NextLevel()
+    {
+      return new StartScreen();
+    }
+
+    public override void Update()
+    {
+      m_frameCount++;
+      h_dispatchKey();
+
+      base.Update();
+
+      IEnumerable<BaseActor> killedActors = CollisionChecher.GetAllCollisions(Actors);
+
+      foreach (BaseActor killedActor in killedActors)
+      {
+        if (killedActor.IsAlive)
+          killedActor.IsAlive = false;
+      }
+
+      List<BaseActor> toRemove = Actors.Where(actor => actor.CanDrop).ToList();
+      BaseActor[] actors = new BaseActor[toRemove.Count()];
+      toRemove.CopyTo(actors);
+
+      foreach (BaseActor actor in actors.Where(actor => actor.CanDrop))
+      {
+        Actors.Remove(actor);
+      }
+
+      if (Player.CanDrop)
+        Failed = true;
+
+      //has no enemy
+      if (Actors.All(actor => actor.ActorType != ActorType.Enemy))
+        Success = true;
+    }
+
+    #endregion
+  }
+}
